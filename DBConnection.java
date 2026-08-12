e3e091e0c6f9a8ebba3e3fc46b4e7afdb1d2680c
Rithish bala <rithishbala2207@gmail.com>
2026-08-12 15:51:28 +0530

    Add DBConnection class for MySQL database access

---
+++ DBConnection.java
@@ -0,0 +1,46 @@
+package exam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/online_exam";
+
+    private static final String USER =
+            "root";
+
+    private static final String PASSWORD =
+            "Yogesad7";
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+
+            System.out.println(
+                    "Database Connected Successfully!"
+            );
+
+        } catch (Exception e) {
+
+            System.out.println(
+                    "Database Connection Failed!"
+            );
+
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
